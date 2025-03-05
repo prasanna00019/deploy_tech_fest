12aab64e61363a309f718628d506987e21b1c2bf
prasanna.000019@gmail.com <prasanna.000019@gmail.com>
2025-03-05 16:12:48 +0530

    DB_PORT

--- Server/db.js
+++ Server/db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.db_PORT,
+    port: process.env.DB_PORT,
     connectTimeout:30000,
 });
 
