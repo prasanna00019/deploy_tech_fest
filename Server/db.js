ed5862b8ec5ad7b2422ad862e5991aa7e4e2a04e
prasanna.000019@gmail.com <prasanna.000019@gmail.com>
2025-03-09 00:33:39 +0530

    SSL

--- Server/db.js
+++ Server/db.js
@@ -1,12 +1,18 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
-
+import fs from "fs";
+import path from "path";
 dotenv.config();
+
 const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    port:process.env.DB_PORT,
+    ssl: {
+      ca: Buffer.from(process.env.MYSQL_CA_CERT, "base64").toString("utf-8")
+  }
 });
 
 db.connect(error => {
